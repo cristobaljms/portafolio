77e9b6a97b46956778e8e72c55d9483d3a42ee9a
Cristobal <cmunoz21x@gmail.com>
2025-01-26 19:18:27 -0400

    ajustes

--- src/app/work/differenziamocolcoure/page.tsx
+++ src/app/work/differenziamocolcoure/page.tsx
@@ -3,14 +3,19 @@ import Image from "next/image";
 import Poster from "@/assets/projects/diffenrenziamo/poster.webp";
 import Image1 from "@/assets/projects/diffenrenziamo/1.webp";
 
-
 export default function Page() {
   return (
     <main className="container mx-auto px-5">
       <Navbar2 />
       <ul className="mb-4">
-        <li>Descripción: Aplicación web diseñada para la creación de concursos y la recaudación de fondos destinados a organizaciones benéficas, proporcionando una plataforma intuitiva y eficiente para apoyar causas sociales.</li>
-        <li>Tecnologías: React.js, CSS, SemanticUI</li>
+        <li>
+          Aplicación web diseñada para la creación de concursos y la recaudación
+          de fondos destinados a organizaciones benéficas, proporcionando una
+          plataforma intuitiva y eficiente para apoyar causas sociales.
+        </li>
+        <li>
+          <b>Tecnologías:</b> React.js, CSS, SemanticUI
+        </li>
       </ul>
       <div>
         <Image src={Poster} alt="" />
